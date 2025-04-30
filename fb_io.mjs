90b78e9620fdcd3ee79ee5b3d8cceaafaa4e649d
Caleb-Lowe1 <22026cl@hvhs.school.nz>
2025-04-30 22:00:25 +0000

    Task 1, 2 and 3 completed

--- fb_io.mjs
+++ fb_io.mjs
@@ -15,15 +15,40 @@ console.log('%c fb_io.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
-
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase }
+from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
 /**************************************************************/
+const firebaseConfig = {
+    apiKey: "AIzaSyCwPibZHntricqhOchcdlX3H7ve_CFQhR0",
+    authDomain: "comp-2025-caleb-lowe-31f01.firebaseapp.com",
+    databaseURL: "https://comp-2025-caleb-lowe-31f01-default-rtdb.firebaseio.com",
+    projectId: "comp-2025-caleb-lowe-31f01",
+    storageBucket: "comp-2025-caleb-lowe-31f01.firebasestorage.app",
+    messagingSenderId: "440676386005",
+    appId: "1:440676386005:web:05b4cb8a914c0ceb0ace5c",
+    measurementId: "G-WGYBNEYVY3"
+  };
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firebaseGameDB = getDatabase(app);
+console.info(firebaseGameDB);
+
+
 export { 
     fb_initialise };
-
+/******************************************************/
+// fb_login()
+// Called by html LOGIN button
+// Login to Firebase via Google authentication
+// Input:  n/a
+// Return: n/a
+/******************************************************/    
 function fb_initialise() {
     console.log('%c fb_initialise(): ', 
                 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
--- main.mjs
+++ main.mjs
@@ -15,7 +15,8 @@ console.log('%c main.mjs',
 import { fb_initialise }
     from './fb_io.mjs';
     window.fb_initialise   = fb_initialise;
- 
+
+   
 /**************************************************************/
 // index.html main code
 /**************************************************************/
